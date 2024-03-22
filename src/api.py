80caf3acce72c9435482d5585e63256152f02f18
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-03-22 19:57:06 +0300

    фикс: пофиксил ошибки от mypy

--- src/api.py
+++ src/api.py
@@ -6,12 +6,12 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root() -> dict:
+async def root() -> dict[str, str]:
     """Возвращает сообщение "Hello World"."""
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str) -> dict:
+async def say_hello(name: str) -> dict[str, str]:
     """Возвращает сообщение "Hello" вместе и "name"."""
     return {"message": f"Hello {name}"}
