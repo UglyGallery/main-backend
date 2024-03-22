1b17513de059bbdccfa065b5cd9d4628347edf13
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-03-22 23:05:21 +0300

    фича: добавил if __name__ == __"main"__

--- src/main.py
+++ src/main.py
@@ -6,11 +6,12 @@ from src.database import async_engine
 from src.database.models.base import Base
 
 
-async def async_main() -> None:
+async def main() -> None:
     """Стартовая точка всего приложения."""
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
-asyncio.run(async_main())
+if __name__ == "__main__":
+    asyncio.run(main())
