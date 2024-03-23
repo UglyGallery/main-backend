176e42da3e55935011f5f9d16c676f49bc6a98b0
kolyaslow <136946988+kolyaslow@users.noreply.github.com>
2024-03-23 18:57:43 +0700

    Update base.py

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -1,26 +1,5 @@
-# from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase
 
-
-# class Base(DeclarativeBase):
-#     """Базовая модель SQLAlchemy, от которой наследуются все остальные."""
 
 class Base(DeclarativeBase):
-    """
-    Создание PK и преобразоывавыние имени таблице из PascalCase в snake_case, дял таблиц БД
-    """
-
-    __abstract__ = True
-    primary_key_id: bool = True
-
-    @declared_attr
-    def id(cls):
-        if cls.primary_key_id:
-            return mapped_column(Integer, primary_key=True)
-        else:
-            return None
-
-    @declared_attr.directive
-    def __tablename__(cls) -> str:
-        words_in_name = re.findall("[A-Z][a-z]*", cls.__name__)
-        snake_case_name = "_".join(word.lower() for word in words_in_name)
-        return snake_case_name
+    """Базовая модель SQLAlchemy, от которой наследуются все остальные."""
