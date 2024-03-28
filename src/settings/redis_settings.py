ff135c7b9760b57ae08e512ea39b531394e76fc9
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-03-28 03:02:05 +0300

    мелочь: добавил настройки для Redis

---
+++ src/settings/redis_settings.py
@@ -0,0 +1,10 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class RedisSettings(BaseSettings):
+    """Хранит настройки, связанные с файловым хранилищем MinIO."""
+
+    port: str = Field(alias="REDIS_PORT")
+    password: str = Field(alias="REDIS_PASSWORD")
+    host: str = Field(alias="REDIS_HOST")
--- src/settings/settings.py
+++ src/settings/settings.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 from src.settings.database_settings import DatabaseSettings
 from src.settings.logging_settings import LoggingSettings
 from src.settings.minio_settings import MinIOSettings
+from src.settings.redis_settings import RedisSettings
 from src.settings.sqlalchemy_settings import SQLAlchemySettings
 
 load_dotenv()
@@ -16,6 +17,7 @@ class Settings(BaseSettings):
     logging: LoggingSettings = LoggingSettings()
     sqlalchemy: SQLAlchemySettings = SQLAlchemySettings()
     minio: MinIOSettings = MinIOSettings()
+    redis: RedisSettings = RedisSettings()
 
 
 settings = Settings()
