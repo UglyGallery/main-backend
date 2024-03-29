de4f20097fecb13c28a829c7dfe3c1c22e984889
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-03-30 00:08:01 +0300

    мелочь: удалил ненужный файл api.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
 from __future__ import annotations
 
 import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from src.api import app
+from src.features import categories, posts
+
+app = FastAPI()
+
+origins = ["*"]
+
+# noinspection PyTypeChecker
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(categories.router)
+app.include_router(posts.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=5000)
