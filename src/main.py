6f00a56a32fc848b041f27959f5f88c01b5bb248
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-03-29 23:43:39 +0300

    мелочь: добавил TODO

--- src/main.py
+++ src/main.py
@@ -6,3 +6,6 @@ from src.api import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=5000)
+
+# TODO: Перенеси инициализацию SQLAlchemy, Redis и Minio в FastAPI Lifespan Events:
+#  https://fastapi.tiangolo.com/advanced/events
