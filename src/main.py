78072768d1245b600e3fb58a3b5256ece09d33f1
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-03-30 02:00:12 +0300

    мелочь: удалил неиспользуемый код

--- src/main.py
+++ src/main.py
@@ -1,25 +1,12 @@
 from __future__ import annotations
 
-from contextlib import asynccontextmanager
-from typing import TYPE_CHECKING
-
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.features import categories, posts
 
-if TYPE_CHECKING:
-    from collections.abc import AsyncGenerator
-
-
-@asynccontextmanager
-async def lifespan(_: FastAPI) -> AsyncGenerator:
-    """Инициализирует общие ресурсы, такие как соединение с БД или Redis."""
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 origins = ["*"]
 
@@ -37,6 +24,3 @@ app.include_router(posts.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=5000)
-
-# TODO: Перенеси инициализацию SQLAlchemy, Redis и Minio в FastAPI Lifespan Events:
-#  https://fastapi.tiangolo.com/advanced/events
